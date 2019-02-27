72ad780256c5ec0ba76948fe2cf49567397ffccd
lzhaoyang <575563079@qq.com>
2019-02-27 23:57:19 +0800

    表添加

--- run.py
+++ run.py
@@ -32,12 +32,25 @@ app = Flask(__name__)
 
 # 数据库模型
 class User(db.Model):
+    """
+    用户表
+    """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
         return f'<User {self.id},{self.username},{self.email}>'
+
+
+class ShadowConf(db.Model):
+    """
+    shadowsocks 配置表
+    """
+    #  主键id
+    id = db.Column(db.Integer, primary_key=True)
+    # 用户id
+    user_id = db.Column(db.Integer, nullable=False)
 
 
 # 是否初始化数据库
